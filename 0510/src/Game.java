f22469cbf1b3afbb88d3cdb1257c333e8effd192
cyberls17 <mhs0001@naver.com>
2022-05-10 10:38:12 +0900

    Edit Game.java

--- 0510/src/Game.java
+++ 0510/src/Game.java
@@ -2,12 +2,32 @@ import java.util.Scanner;
 public class Game {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int assertive = (int)(Math.random()*3+1);
+		int i = (int)(Math.random()*3+1);
 		System.out.println("가위 바위 보!");
-		String input = scan.nextLine();
-		if(assertive == 1)
-		{
+		String a = scan.next();
+		if(i==0)
 			System.out.println("가위");
+		else if(i==1)
+			System.out.println("바위");
+		else if(i==2)
+			System.out.println("보");
+		
+		if(a.equals("가위")) {
+			if(i==0)
+				System.out.println("비겼다");
+			else if(i==1)
+				System.out.println("졌다");
+			else if(i==2)
+				System.out.println("이겼다");
+		}
+		
+		if(a.equals("바위")) {
+			if(i==0)
+				System.out.println("졌다");
+			else if(i==1)
+				System.out.println("비겼다");
+			else if(i==2)
+				System.out.println("이겼다");
 		}
 	}
 }
